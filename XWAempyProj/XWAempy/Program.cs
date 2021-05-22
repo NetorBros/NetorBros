2b567a7007d1b59367941f52798f2ff0ec532879
NetorBros <81164358+NetorBros@users.noreply.github.com>
2021-05-22 16:21:28 +0200

    Add files via upload

---
+++ XWAempyProj/XWAempy/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace XWAempy
+{
+    class Program
+    {
+        public static bool ChrSysPro;
+
+        public static string path = @"C:/Windows Ended";
+        public static string pathF = @"C:/Windows Ended/XWE.data";
+
+        [DllImport("Kernel32.dll")]
+
+        static extern IntPtr XWESystemOperating();
+
+        [DllImport("Kernel32.dll")]
+
+        static extern IntPtr GetXWEOperratingRate(string PathXWE, string RateName);
+
+        [DllImport("Kernel32.dll")]
+
+        static extern IntPtr XWETmbCenter(bool CHRPro, string pathXWE);
+
+        [DllImport("hal.dll")]
+
+        static extern IntPtr PTIsystemXV();
+
+        [DllImport("hal.dll")]
+
+        static extern Int64 PTIsystemProting(string PTIport);
+
+        [DllImport("hal.dll")]
+
+        static extern IntPtr PTIBootError(string path, int ptiport, PTIHsx.PtiErrorBoot bOOT_ERROR_SYS_HANDLE);
+
+        public void XWEbin(int XWE)
+        {
+            int XWEEV = Environment.TickCount.CompareTo(XWE);
+
+            var resand = GetXWEOperratingRate(path, "EnptitiesHK");
+
+            byte xweByt;
+
+            for (int xwe = 0; xwe == 256; XWE++)
+            {
+                xweByt = 255;
+
+                Console.WriteLine($"Byt: {xweByt}...");
+                Console.Clear();
+                Thread.Sleep(150);
+            }
+
+        }
+
+        private static void DnSon(int PTIport, string pathPTI, bool HandleOvn, int PTI, int PTIMpcModel)
+        {
+            pathPTI = "C:/windows/system32/PTI.DAT";
+            PTIport = 355;
+
+
+            try
+            {
+                for (PTI = 0; PTI < 0x8542; PTI++)
+                {
+                    PTIport.GetType();
+                    Type.Delimiter.ToString();
+                }
+            }
+            finally
+            {
+                Console.WriteLine($"port: {PTIport}");
+            }
+
+            switch (PTI)
+            {
+                case 1:
+                    if (PTI == 0x00009f)
+                    {
+                        PTIBootError(pathPTI, PTIport, PTIHsx.PtiErrorBoot.BOOT_ERROR_SYS_HANDLE);
+                    }
+
+                    else if (PTI == 0x00025f)
+                    {
+                        PTIBootError(pathPTI, PTIport, PTIHsx.PtiErrorBoot.BOOT_ERROR_HARDWARE_IPS);
+                    }
+
+                    if (PTI == 0x02350c)
+                    {
+                        PTIBootError(pathPTI, PTIport, PTIHsx.PtiErrorBoot.BOOT_PTI_FILE_NOT_EXIST);
+                    }
+                    break;
+            }
+
+        }
+    }
+}
---
+++ XWAempyProj/XWAempy/ProgramPC.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace XWAempy
+{
+    class ProgramPC
+    {
+        public static string DecryptFolde = @"C:/Windows Ended/Decrypt";
+        public static string EncryptFolder = @"C:/Windows Ended/Encrypt";
+        public static string FileOriginal = "TsData.txt";
+        public static string FileEncrypt = "TsData.enc";
+
+        public static void MainX509Cerficate(string certificateName)
+        {
+            StreamWriter streamWriter = File.CreateText(FileOriginal);
+            streamWriter.Write($"{FileOriginal}...");
+            streamWriter.Flush();
+            streamWriter.Close();
+
+            X509Certificate2 certificate = new X509Certificate2();
+            if (certificate == null)
+            {
+                Console.Write("cerificate Not found!");
+                Console.Read();
+            }
+
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                X509Certificate2Collection x509CertificateCollection = store.Certificates;
+                X509Certificate2Collection currentCetificate = x509CertificateCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
+                X509Certificate2Collection signingCertificate = currentCetificate.Find(X509FindType.FindBySubjectDistinguishedName, certificateName, false);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+    }
+
+    class PTIHsx
+    {
+        public enum PtiErrorBoot
+        {
+            BOOT_ERROR_SYS_HANDLE = 0x00009f,
+            BOOT_ERROR_HARDWARE_IPS = 0x00025f,
+            BOOT_PTI_FILE_NOT_EXIST = 0x02350c,
+        }
+    }
+}
